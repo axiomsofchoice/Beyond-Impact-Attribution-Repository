40688dbd3e3064e1f1ad529a0cabb390933a2c88
Ana Nelson <nelson.ana@gmail.com>
2011-05-10 15:56:37 +0100

    Add outline of dexy documentation.

---
+++ populating-fluid.py
@@ -0,0 +1,22 @@
+### @export "imports"
+import fluidinfo
+import os
+
+### @export "fluid-login"
+USERNAME = os.environ["FLUID_USERNAME"]
+PASSWORD = os.environ["FLUID_PASSWORD"]
+
+if not USERNAME:
+    raise Exception("Please supply a fluid account username")
+
+if not PASSWORD:
+    raise Exception("Please supply a fluid account password")
+
+fluidinfo.login(USERNAME, PASSWORD)
+
+headers, response = fluidinfo.call('GET', "/users/%s" % USERNAME)
+
+print headers
+print response
+
+
---
+++ starting-with-fluid.md
@@ -0,0 +1,9 @@
+This is a script to run our demo in fluid.
+
+{{ d['populating-fluid.py|idio|pycon|pyg']['imports'] }}
+
+We log in with a username/password from the env:
+
+{{ d['populating-fluid.py|idio|pycon|pyg']['fluid-login'] }}
+
+
