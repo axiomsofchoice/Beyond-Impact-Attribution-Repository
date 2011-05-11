97b8144e020fc18f6de3225bd42959c149ded47b
Ana Nelson <nelson.ana@gmail.com>
2011-05-11 16:06:51 +0100

    Collection add/list works now working.

--- biar/collection.py
+++ biar/collection.py
@@ -1,4 +1,9 @@
 import fluidinfo
+import pprint
+
+def pp(text):
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(text)
 
 class Collection:
     """The class encapsulated the collection of works that a user can put together for a certain purpose."""
@@ -25,7 +30,7 @@ class Collection:
             raise Exception("unexpected status code %s" % headers['status'])
 
     def init_in_fluid(self, name, description):
-        print "creating namespace..."
+        pp("creating namespace...")
         namespace_info = {
             'name': name,
             'description': description
@@ -34,28 +39,34 @@ class Collection:
         # TODO check headers
         self.fluid_id = response['id']
         self.fluid_url = response['URI']
+        pp(headers)
+        pp(response)
 
-        print "creating element tag in namespace..."
+
         tag_info = {
             'name' : 'element',
             'description' : 'Tag indicating that object is part of a list',
             'indexed' : False
         }
-        headers, response = fluidinfo.call('POST', self.collection_namespace(), tag_info)
+        headers, response = fluidinfo.call('POST', "/tags/%s/biar/collections/%s"
+                                           % (self.connection.username,
+                                              self.name), tag_info)
         # TODO check headers
+        pp(headers)
+        pp(response)
 
     def add_work(self, work):
-        tag_path = "/objects/%s/%s/%s" % (work.object_id, self.collection_namespace(), 'element')
+        tag_path = "/objects/%s/%s/biar/collections/%s/%s" % (work.object_id,
+                                                              self.connection.username,
+                                                              self.name, 'element')
         headers, response = fluidinfo.call('PUT', tag_path)
-        print headers
-        print response
+        # TODO check headers
 
     def list_works(self):
         querystring = "has %s/biar/collections/%s/element" % (self.connection.username, self.name)
         headers, response = fluidinfo.call('GET', '/objects', query=querystring)
-        print headers
-        print response
-        return response
+        # TODO check headers
+        return response['ids']
 
     def parent_namespace(self):
         return "/namespaces/%s/biar/collections" % (self.connection.username)
