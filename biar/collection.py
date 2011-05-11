93a87e2ce071db8a1d2f91b339c131f922028704
Ana Nelson <nelson.ana@gmail.com>
2011-05-11 14:47:58 +0100

    Flesh out work.py, still not implemented.

--- biar/collection.py
+++ biar/collection.py
@@ -1,9 +1,5 @@
 class Collection:
-    """The class encapsulated the collection of works that a user can put together for a certain purpose.
-    """
-    def __init(self,description):
-        pass
-
+    """The class encapsulated the collection of works that a user can put together for a certain purpose."""
     @classmethod
     def fetch(klass, name):
         """Create or retrieve a collection with a given name."""
@@ -13,4 +9,7 @@ class Collection:
     def AddWork(self,workToAdd):
         """Creates a new list membership with a work and adds it to this list.
         """
+        pass
+
+    def collection_namespace(klass, name):
         pass
