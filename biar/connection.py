636891a60e84d3744bcac3bed46196a5b927e87b
Ana Nelson <nelson.ana@gmail.com>
2011-05-11 15:22:27 +0100

    Write collection init methods.

--- biar/connection.py
+++ biar/connection.py
@@ -3,17 +3,18 @@ import fluidinfo
 class Connection:
     @classmethod
     def connect(klass, username, password):
+        fluidinfo.login(username, password)
         conn = klass()
         conn.username = username
-        fluidinfo.login(username, password)
+        conn.setup_biar_namespaces()
         return conn
+
+    def setup_biar_namespaces(self):
+        pass
 
     def user_info(self):
         headers, response = fluidinfo.call('GET', "/users/%s" % self.username)
         return response
-
-    def user_name(self):
-        return self.user_info()['name']
 
     def user_id(self):
         return self.user_info()['id']
