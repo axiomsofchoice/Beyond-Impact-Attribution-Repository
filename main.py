8066ad63e9f6882f92cc5a56627447e5c46fc1af
Sander van der Waal <sandervanderwaal@yahoo.com>
2011-05-11 14:57:06 +0100

    First version of cli

---
+++ main.py
@@ -0,0 +1,83 @@
+### @export "imports"
+import sys
+import getpass
+import fluidinfo
+import json
+import os
+import urllib
+
+LIST_TAG = 'biar/collection'
+
+def printListItems(list_name):
+    LIST_TAG = 'biar/collection'
+    headers, response = fluidinfo.call('GET', '/objects', query='%s matches "%s"' % (LIST_TAG, list_name))
+    print response
+
+    return 
+
+
+def printAllLists():
+    headers, response = fluidinfo.call('GET', '/objects', query='has %s' % LIST_TAG)
+
+    for obj_id in response['ids']:
+        object_tag_path = "/objects/%s/%s" % (obj_id, LIST_TAG)
+        details_h, details_response = fluidinfo.call('GET', object_tag_path)
+        print details_response
+    return 
+
+def logIn():
+    print
+    print "Welcome to Beyond Impact Attribute Repository (BIAR)."
+    print
+    username = raw_input("Please enter your fluidinfo username : ")
+    password = getpass.getpass("Enter your fuidinfo password: ")
+
+    if not username:
+        raise Exception("Please supply a fluid account username")
+
+    if not password:
+        raise Exception("Please supply a fluid account password")
+
+    try:
+        fluidinfo.login(username, password)
+        headers, response = fluidinfo.call('GET', "/users/%s" % username)
+        if type(response) is str:
+            errorMessage = "Log in failed for user %s, message: %s" % (username, response)
+            raise Exception(errorMessage)
+        else:
+            print 
+            print "Welcome",response['name']
+            print
+    except Exception as e:
+        raise e	
+    return
+
+def getAndRunCommand():
+    full_cmd = raw_input("Choose command (lists, elements <listid>, exit): ").split()
+    cmd = full_cmd[0]
+    print
+
+    if cmd == "lists":
+	    printAllLists();
+    elif cmd == "elements":
+	    printListItems(full_cmd[1]);
+    elif cmd == "exit":
+        print "Thanks for using BIAR. Now exiting..."
+        sys.exit();
+    else:
+        print "Command",cmd,"not recognised."
+    return
+	
+def main():
+    try:
+        logIn()
+        while(True):
+            getAndRunCommand()
+            print
+
+    except Exception as e:
+        print(str(e))
+        raise e
+
+if __name__ == "__main__":
+    main()
