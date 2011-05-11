04e3dd36302e54aad6d06a670803ff418e0b6812
Ian Mulvany <Ian@Mulvany.net>
2011-05-11 15:27:06 +0100

    seeks grant ids via entrz

--- parse-f1000.py
+++ parse-f1000.py
@@ -1,7 +1,9 @@
-
-
+import BeautifulSoup
 from BeautifulSoup import BeautifulStoneSoup
+import json
 import urllib2
+
+
 file_location = "/Users/ian/sandbox/f1000/small-f1000.xml"
 xml = open(file_location, "r").read()
 soup = BeautifulStoneSoup(xml)
@@ -18,13 +20,29 @@ def convert(doi):
             # strip of part after first _!
             return pmid
 
-
-
+def pmid2grant(pmid):
+    # generate a query session key
+    url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/epost.fcgi?db=pubmed&id=%s" % pmid
+    pxml = urllib2.urlopen(url).read()
+    psoup =  BeautifulStoneSoup(pxml)
+    query_key = psoup.querykey.contents[0]
+    webenv = psoup.webenv.contents[0]
+    # query for the grant info
+    url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&query_key=%s&WebEnv=%s&retmode=xml" % (query_key, webenv)
+    gxml = urllib2.urlopen(url).read()
+    soup = BeautifulStoneSoup(gxml)
+    try:
+        grant = soup.GrantID.contents[0]
+        print grant 
+        return grant
+    except:
+        return "no grant"
 articles = soup.findAll('article')
 for article in articles:
     doi =  article.doi.contents[0]
     pmid = convert(doi)
-    print doi, pmid
+    grant = pmid2grant(pmid)
+    print doi, pmid, grant
     #print article.rating.contents[0]
     #print article.title.contents[0]
 ### lookup mendleey info my doi
