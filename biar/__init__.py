e1ee5181e3085cf795435908372ad4615ef144ad
Ana Nelson <nelson.ana@gmail.com>
2011-05-11 14:33:04 +0100

    Split biar.py into separate files. Add setup.py file. Add connection example.

---
+++ biar/__init__.py
@@ -0,0 +1,2 @@
+"""This collection of classes provides the core abstration of the Beyond Impact Attribution Repository
+"""
---
+++ biar/collection.py
@@ -0,0 +1,16 @@
+class Collection:
+    """The class encapsulated the collection of works that a user can put together for a certain purpose.
+    """
+    def __init(self,description):
+        pass
+
+    @classmethod
+    def fetch(klass, name):
+        """Create or retrieve a collection with a given name."""
+
+        pass
+
+    def AddWork(self,workToAdd):
+        """Creates a new list membership with a work and adds it to this list.
+        """
+        pass
